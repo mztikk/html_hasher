fcb476fa14ab26637c89cf624a3d61af0cc77e80
Nils Feierabend <mztikk@outlook.de>
2021-10-20 20:32:41 +0200

    xxh

--- src/xxHash.rs
+++ src/xxHash.rs
@@ -1,3 +1,5 @@
+// https://github.com/DoumanAsh/xxhash-rust/tree/e110e60b85c5cef2de37191ce00d323a7bc0f569
+
 use std::mem;
 
 const PRIME32_1: u32 = 2654435761;
@@ -16,14 +18,14 @@ const fn get_32bits(input: &[u8], cursor: usize) -> u32 {
         | (input[cursor + 3] as u32) << 24
 }
 
-#[inline]
+#[inline(always)]
 const fn round(acc: u32, input: u32) -> u32 {
     acc.wrapping_add(input.wrapping_mul(PRIME32_2))
         .rotate_left(13)
         .wrapping_mul(PRIME32_1)
 }
 
-#[inline]
+#[inline(always)]
 const fn avalanche(mut input: u32) -> u32 {
     input ^= input >> 15;
     input = input.wrapping_mul(PRIME32_2);
@@ -33,26 +35,7 @@ const fn avalanche(mut input: u32) -> u32 {
     input
 }
 
-const fn finalize(mut input: u32, data: &[u8], mut cursor: usize) -> u32 {
-    let mut len = data.len() - cursor;
-
-    while len >= 4 {
-        input = input.wrapping_add(get_32bits(data, cursor).wrapping_mul(PRIME32_3));
-        cursor += mem::size_of::<u32>();
-        len -= mem::size_of::<u32>();
-        input = input.rotate_left(17).wrapping_mul(PRIME32_4);
-    }
-
-    while len > 0 {
-        input = input.wrapping_add((data[cursor] as u32).wrapping_mul(PRIME32_5));
-        cursor += mem::size_of::<u8>();
-        len -= mem::size_of::<u8>();
-        input = input.rotate_left(11).wrapping_mul(PRIME32_1);
-    }
-
-    avalanche(input)
-}
-
+#[inline(always)]
 pub const fn xx_hash32(input: &[u8]) -> u32 {
     xx_hash32_seed(input, 0)
 }
@@ -92,5 +75,21 @@ pub const fn xx_hash32_seed(input: &[u8], seed: u32) -> u32 {
         result = result.wrapping_add(seed.wrapping_add(PRIME32_5));
     }
 
-    finalize(result, input, cursor)
+    let mut len = input.len() - cursor;
+
+    while len >= 4 {
+        result = result.wrapping_add(get_32bits(input, cursor).wrapping_mul(PRIME32_3));
+        cursor += mem::size_of::<u32>();
+        len -= mem::size_of::<u32>();
+        result = result.rotate_left(17).wrapping_mul(PRIME32_4);
+    }
+
+    while len > 0 {
+        result = result.wrapping_add((input[cursor] as u32).wrapping_mul(PRIME32_5));
+        cursor += mem::size_of::<u8>();
+        len -= mem::size_of::<u8>();
+        result = result.rotate_left(11).wrapping_mul(PRIME32_1);
+    }
+
+    avalanche(result)
 }
